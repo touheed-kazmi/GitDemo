b12ab1ae12aad9d97a2e3bf11ab0893acd3022ff
touheed <touheed.kazmi@accenture.com>
2024-11-16 15:44:57 +0530

    first commit

---
+++ src/test/java/GitTest/GitTest/test/TestGit.java
@@ -0,0 +1,15 @@
+package GitTest.GitTest.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://test.salesforce.com");
+	}
+
+}
