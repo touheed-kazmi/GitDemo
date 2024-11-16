6f04324a626edab2cdd56154e730c351b0ac7494
touheed <touheed.kazmi@accenture.com>
2024-11-16 16:00:52 +0530

    this is commit from Asian Timezone guy

--- src/test/java/GitTest/GitTest/test/TestGit.java
+++ src/test/java/GitTest/GitTest/test/TestGit.java
@@ -8,8 +8,13 @@ public class TestGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		WebDriver driver = new FirefoxDriver();
-		driver.get("https://test.salesforce.com");
+//		WebDriver driver = new FirefoxDriver();
+//		driver.get("https://test.salesforce.com");
+		
+		System.out.println("test git1");
+		System.out.println("test git2");
+		System.out.println("test git3");
+		
 	}
 
 }
